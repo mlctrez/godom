d57a4c1bc8d0cc58f507121a2f83027d9529cfe3
mlctrez <mlctrez@gmail.com>
2023-09-10 18:01:50 -0500

    add periodic utility function

---
+++ gdutil/periodic.go
@@ -0,0 +1,33 @@
+package gdutil
+
+import (
+	"context"
+	"time"
+)
+
+type PeriodicFunc func() (ok bool)
+
+// Periodic executes a PeriodicFunc every interval.
+// It will return if context is cancelled or PeriodicFunc returns false.
+// It is intended to be run as a go routine.
+//
+//	go Periodic(ctx, time.Second, func() bool {
+//	  err := ErrorsWhenDone()
+//	  // no error means continue
+//	  return err == nil
+//	})
+func Periodic(ctx context.Context, interval time.Duration, pf PeriodicFunc) {
+	pingTicker := time.NewTicker(interval)
+	defer pingTicker.Stop()
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-pingTicker.C:
+			if pf() {
+				continue
+			}
+			return
+		}
+	}
+}
---
+++ gdutil/periodic_test.go
@@ -0,0 +1,35 @@
+package gdutil
+
+import (
+	"context"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestPeriodic(t *testing.T) {
+
+	a := assert.New(t)
+
+	ctx, cancelFunc := context.WithCancel(context.TODO())
+	cancelFunc()
+	// already cancelled func should not call shouldContinue
+	a.NotPanics(func() {
+		Periodic(ctx, time.Millisecond, func() (ok bool) { panic("PeriodicFunc") })
+	})
+
+	ctx, cancelFunc = context.WithCancel(context.TODO())
+	defer cancelFunc()
+	shouldContinue := true
+	invokeCount := 0
+	go Periodic(ctx, time.Millisecond, func() (ok bool) {
+		invokeCount++
+		return shouldContinue
+	})
+	time.Sleep(2 * time.Millisecond)
+	shouldContinue = false
+	time.Sleep(2 * time.Millisecond)
+	a.True(invokeCount > 0)
+	fmt.Println(invokeCount)
+}
