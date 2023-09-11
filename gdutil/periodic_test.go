1194b6620e040e070e4937ed06e1955a429802c9
mlctrez <mlctrez@gmail.com>
2023-09-10 23:09:19 -0500

    attempt to fix timing issue on periodic test

--- gdutil/periodic_test.go
+++ gdutil/periodic_test.go
@@ -2,6 +2,7 @@ package gdutil
 
 import (
 	"context"
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
@@ -11,27 +12,26 @@ func TestPeriodic(t *testing.T) {
 
 	a := assert.New(t)
 
-	ctx, cancelFunc := context.WithCancel(context.TODO())
+	ctx, cancelFunc := context.WithCancel(context.Background())
 	cancelFunc()
 	// already cancelled func should not call shouldContinue
 	a.NotPanics(func() {
 		Periodic(ctx, time.Millisecond, func() (ok bool) { panic("PeriodicFunc") })
 	})
 
-	ctx, cancelFunc = context.WithCancel(context.TODO())
+	ctx, cancelFunc = context.WithCancel(context.Background())
 	defer cancelFunc()
 	shouldContinue := true
 	invokeCount := 0
 	go Periodic(ctx, time.Millisecond, func() (ok bool) {
+		fmt.Println("invokeCount", invokeCount)
 		invokeCount++
 		return shouldContinue
 	})
 	for invokeCount < 2 {
-		time.Sleep(1 * time.Millisecond)
+		time.Sleep(10 * time.Millisecond)
 	}
 	shouldContinue = false
-	for invokeCount < 4 {
-		time.Sleep(1 * time.Millisecond)
-	}
+	time.Sleep(10 * time.Millisecond)
 	a.True(invokeCount > 0)
 }
