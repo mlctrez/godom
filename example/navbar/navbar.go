713be1a5ac7c7c3143f4a9f6107fbfcfabac619c
mlctrez <mlctrez@gmail.com>
2023-09-22 20:59:48 -0500

    simplify, not there yet but better

--- example/navbar/navbar.go
+++ example/navbar/navbar.go
@@ -39,6 +39,7 @@ type navItem struct {
 }
 
 func Render(ctx *app.Context) godom.Element {
+	// TODO: simplify making a new doc context with callback
 	doc := godom.Doc{Doc: godom.Document().This()}
 	nav := &navbar{}
 	doc.CallBack = callback.Reflect(nav)
@@ -52,24 +53,25 @@ func Render(ctx *app.Context) godom.Element {
 		{Name: "Page", Path: "/page"},
 	}
 
+	var navItemFmt = `<li class="nav-item"><a role="button" class="nav-link">%s</a></li>`
+
+	// TODO: this is messy and a bit more code than expected. find a way to simplify.
 	for _, outer := range navItems {
 		item := outer
-		li := doc.El("li", doc.At("class", "nav-item"))
-		a := doc.El("a", doc.At("role", "button"))
-		li.AppendChild(a)
-		a.AddEventListener("click", func(event godom.Value) {
+
+		ni := doc.H(fmt.Sprintf(navItemFmt, item.Name))
+		// TODO: add navigation to new location with event listener func
+		ni.ChildNodes()[0].AddEventListener("click", func(event godom.Value) {
 			u, _ := url.Parse(item.Path)
-			fmt.Println("click", u)
 			ctx.Events <- &app.Location{URL: u}
 		})
 		if ctx.URL.Path == item.Path {
-			a.SetAttribute("class", "nav-link active")
-			a.SetAttribute("aria-current", "page")
-		} else {
-			a.SetAttribute("class", "nav-link")
+			// TODO: get element from child nodes without cast
+			element := ni.ChildNodes()[0].(godom.Element)
+			element.SetAttribute("class", "nav-link active")
+			element.SetAttribute("aria-current", "page")
 		}
-		a.AppendChild(doc.T(item.Name))
-		nav.NavItems.AppendChild(li)
+		nav.NavItems.AppendChild(ni)
 	}
 
 	nav.Form.AddEventListener("submit", nav.form(ctx))
