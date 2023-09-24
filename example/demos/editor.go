dd97f4b9e49811058c2f685534151dcb048067c9
mlctrez <mlctrez@gmail.com>
2023-09-23 23:02:07 -0500

    editor updates on load

--- example/demos/editor.go
+++ example/demos/editor.go
@@ -19,25 +19,30 @@ type editor struct {
 
 func (e *editor) bind(ctx *app.Context) {
 	e.Textarea.AddEventListener("change", func(event godom.Value) {
-		for _, node := range e.Target.ChildNodes() {
-			e.Target.RemoveChild(node.This())
-		}
-		s := event.Get("target").Get("value").String()
-		dec, err := ctx.Doc.Decode(xml.NewDecoder(bytes.NewBufferString(s)))
-		e.Target.AppendChild(ctx.Doc.H(s))
-		if err == nil {
-			// reformat good data back into the text area
-			encoder := godom.NewEncoder(&bytes.Buffer{})
-			encoder.Indent("  ")
-			s = dec.Marshal(encoder).Xml()
-			event.Get("target").Set("value", s)
-		}
+		e.update(ctx)
 	})
+}
+
+func (e *editor) update(ctx *app.Context) {
+	for _, node := range e.Target.ChildNodes() {
+		e.Target.RemoveChild(node.This())
+	}
+	s := e.Textarea.This().Get("value").String()
+	dec, err := ctx.Doc.Decode(xml.NewDecoder(bytes.NewBufferString(s)))
+	e.Target.AppendChild(ctx.Doc.H(s))
+	if err == nil {
+		// reformat good data back into the text area
+		encoder := godom.NewEncoder(&bytes.Buffer{})
+		encoder.Indent("  ")
+		s = dec.Marshal(encoder).Xml()
+		e.Textarea.This().Set("value", s)
+	}
 }
 
 func Editor(ctx *app.Context) godom.Element {
 	ed := &editor{}
 	el := ctx.Doc.WithCallback(callback.Reflect(ed)).H(editorHtml)
 	ed.bind(ctx)
+	ed.update(ctx)
 	return el
 }
